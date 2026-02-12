e070a112cf11b5f5f57b024258d069790d34dbab
Deba Karan Agrawala <karan.deba@gmail.com>
2026-02-12 17:57:08 +0530

    feat(app): render dynamic section titles from content data
    - Import content configuration array
    - Map over content to display section titles
    - Integrate dynamic rendering within Container layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import Header from "./components/Header"
 import { Container } from "./components/styled/Container.styled"
 import { theme } from "./styled/theme"
 import GlobalStyles from "./styled/Global"
+import content from "./content"
 
 function App() {
 
   return (
     <ThemeProvider theme={theme}>
        <>
-       <GlobalStyles />
+          <GlobalStyles />
           <Header />
           <Container>
-              <h1>Hello World</h1>
+              {content.map((item) => (
+                <p key={item.id}>{item.title}</p>
+              ))}
             </Container>
         </>
     </ThemeProvider>
