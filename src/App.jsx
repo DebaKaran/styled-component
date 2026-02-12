898d369be6d7d1f26eb569110c0ecd0858c995c2
Deba Karan Agrawala <karan.deba@gmail.com>
2026-02-12 13:48:43 +0530

    feat(header): add StyledHeader component
    - Create StyledHeader using styled-components
    - Apply background color and vertical padding

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import Header from "./components/Header"
 import { Container } from "./components/styled/Container.styled"
 
 function App() {
 
   return (
-    <Container>
-      <h1>Hello World</h1>
-    </Container>
+    <>
+      <Header />
+      <Container>
+          <h1>Hello World</h1>
+        </Container>
+    </>
+    
   )
 }
 
