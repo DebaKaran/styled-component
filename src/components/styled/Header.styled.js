d1c986283c5f6be0684d1269c9959bd375410dad
Deba Karan Agrawala <karan.deba@gmail.com>
2026-02-12 14:34:51 +0530

    feat(header): add dynamic background support via props
    - Update StyledHeader to accept bg prop
    - Implement conditional background-color using props
    - Apply custom background in Header component

--- src/components/styled/Header.styled.js
+++ src/components/styled/Header.styled.js
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 
 export const StyledHeader = styled.header`
-    background-color: #ebfbff;
+    background-color: ${({bg}) => bg ? bg : "#ebfbff"};
+    
     padding : 40px 0;
 
     /* h1 {
