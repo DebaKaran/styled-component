c27e6fe90b766acfa56b5a289aa07e8803cbea08
Deba Karan Agrawala <karan.deba@gmail.com>
2026-02-12 15:50:09 +0530

    feat(styling): add global styles using createGlobalStyle
    - Create GlobalStyles component for app-wide CSS reset
    - Apply theme-based body background color
    - Move base typography and layout rules to styled-components
    - Render GlobalStyles inside ThemeProvider

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { ThemeProvider } from "styled-components"
 import Header from "./components/Header"
 import { Container } from "./components/styled/Container.styled"
 import { theme } from "./styled/theme"
+import GlobalStyles from "./styled/Global"
 
 function App() {
 
   return (
     <ThemeProvider theme={theme}>
        <>
+       <GlobalStyles />
           <Header />
           <Container>
               <h1>Hello World</h1>
